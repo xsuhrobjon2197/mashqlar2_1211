3a18845960e31457d8b9cd49a0406a99e0cd511c
xsuhrobjon2197 <xsuhrobjon2197@gmail.com>
2025-11-12 05:15:18 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#8-misol
+i = "Men python o'rganaman"
+x = i.replace("python","Java")
+print(x)
+
+#9-misol
+i = input("tel raqamni kirit:")
+x = i.replace("-"," ")
+print(x)
+
+#10-misol
+i = "salom dunyo salom dunyo"
+x = i.replace(" ","_")
+print(x)
+
+#11-misol
+i = input("Gap kiriting: ")
+x = i.find("salom")
+print(x)
+
+#12-misol
+matn = input("matn kirit: ")
+q = input("q so'zni kirit: ")
+c = matn.find(q)
+print(c)
